e9a272681481661ee66e1907b1d17024439ce043
nick6064 <nitinyelwande6064@gmail.com>
2018-07-26 14:36:46 +0530

    carousel done

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,15 @@
 $(function(){
   new WOW().init();
 });
+/*******************
+    Team
 
+********************/
 $(function(){
-  $("team-members").owlCorousal();
+  $("#team-members").owlCarousel({
+    item:3,
+    autoplay:true,
+    smartSpeed:700,
+    loop:true
+  });
 });
